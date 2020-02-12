2f02416e137d81263a31be17ad7f1bfd4f68a1bf
StasVitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-02-12 06:26:50 -0800

    feat: add recursion tasks and tests

---
+++ src/recursive_tasks.test.ts
@@ -0,0 +1,46 @@
+import {sumTo, recurSum, formulaSum,factorialRecur,factorialLoop,fibonachi,fibonachiLoop,traverseList} from "./recursive_tasks"
+describe("tests for the recursive_tasks functions", () => {
+    it("returns the sum of all the numbers in the arithmetic sequence(a sequence of numbers which differ from each other by a common difference) using looping", () => {
+        expect(sumTo(4)).toBe(10);
+    });
+    it("returns the sum of all of the numbers in the arithmetic sequence recursively", () => {
+        expect(recurSum(4)).toBe(10);
+    });
+    it("returns the sum of all of the numbers in the arithmetic sequence using the formula for arithmetic progression", () => {
+        expect(formulaSum(4)).toBe(10);
+    });
+    it("recursively returns factorial of the passed number", () => {
+        expect(factorialRecur(4)).toBe(24);
+    });
+    it("returns the factorial of the passed number using looping", () => {
+        expect(factorialLoop(4)).toBe(24);
+    });
+    it("recursively returns the fibonachi for the number", () => {
+        expect(fibonachi(7)).toBe(13);
+    });
+    it("returns the fibonachi for the number using looping", () => {
+        expect(fibonachiLoop(7)).toBe(13);
+    })
+});
+describe("tests for the traverseList function", () => {
+    it("traverses the list", () => {
+        let list = {
+            value: 1,
+            next: {
+                value: 2,
+                next: {
+                    value: 3,
+                    next: {
+                        value: 4,
+                        next: null
+                    }
+                }
+            }
+        };
+        let sum = 0;
+        traverseList(list, (element) => {
+            return sum += element;
+        })
+        expect(sum).toBe(10);
+    })
+})
---
+++ src/recursive_tasks.ts
@@ -0,0 +1,81 @@
+export const sumTo = (n) => {
+    let sum = n;
+    for(let i = n-1; i >= 0; i--) {
+        sum += i;
+    }
+    return sum;
+}
+//console.log(sumTo(4));
+export const recurSum = (n) => {
+    let sum = n;
+    if(n == 1) {
+        return 1;
+    }
+    return sum += recurSum(n-1);
+}
+//console.log(recurSum(4));
+export const formulaSum = (n) => {
+    return (n*(n+1))/2;
+}
+//console.log(formulaSum(4));
+export const factorialRecur = (n) => {
+    if(n ==1) {
+        return 1;
+    }
+    return n * factorialRecur(n-1);
+}
+export const factorialLoop = (n) => {
+    let arr = [1,1];
+    for(let i = 2; i <= n; i++) {
+        arr.push(i* arr[i - 1]);
+    }
+    return arr[arr.length -1];
+}
+/* let time = performance.now();
+ console.log(factorialRecur(11));
+ console.log("recursive solution:", performance.now() - time);
+ let time2 = performance.now()
+ console.log(factorialLoop(11));
+ console.log("Looping Factorial: ", performance.now() - time2); */
+
+export const fibonachi = (n) => {
+    if(n == 1) {
+        return 1;
+    }
+    if(n == 0) {
+        return 0;
+    }
+    return fibonachi(n-1) + fibonachi(n-2);
+}
+export const fibonachiLoop = (n) => {
+    let arr = [0,1];
+    for(let i = 2; i <= n; i++) {
+        arr.push((arr[i-1]) + (arr[i-2]))
+    }
+    return arr[arr.length -1];
+}
+// console.log(fibonachiLoop(7));
+// console.log(fibonachi(7));
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+export const traverseList = (list, callback) => {
+    while(list != null) {
+        callback(list.value);
+        list = list.next;
+    }
+}
+
+/* console.log(traverseList(list, (element) => {
+ console.log(element);
+ })); */
